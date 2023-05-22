5b5870fa7701420298fd5536bc2a088a2525075f
gea-jose <geajose425@gmail.com>
2023-05-22 23:40:38 +0530

    incorporated review comments

--- blog/database.py
+++ blog/database.py
@@ -3,9 +3,21 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 
-engine = create_engine("sqlite:///./blog.db", connect_args={"check_same_thread": False})
+
+
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:mysuperpassword@localhost/tribe"
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    except:
+        db.close()
 
--- blog/schemas.py
+++ blog/schemas.py
@@ -1,7 +1,7 @@
 from pydantic import BaseModel
 from datetime import date
 
-class Blog(BaseModel):
+class Employee(BaseModel):
     employee_id:int
     name:str
     joining_date : date
